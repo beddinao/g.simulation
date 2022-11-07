bb16da75aed905b524a2a412fe987f28954884c8
ed-dinaoui <80011820+ed-dinaoui@users.noreply.github.com>
2022-11-07 18:01:46 -0500

    5 files modified

--- src/canvas/Canvas.js
+++ src/canvas/Canvas.js
@@ -2,6 +2,7 @@ import React , { useEffect } from 'react' ;
 import { objects , pop_manif } from '../objects/objects' ;
 import { tar } from '../windows/Sides' ;
 import { move } from '../components/cursor/cursor' ;
+import { scale } from '../windows/Options' ;
 import './Canvas.css' ;
 
 export var ctx , canvas_events , rec_size = [ 0 , 1 ] ;
@@ -57,19 +58,19 @@ function Canvas () {
 
     let mass_i = objects.get_masses()[tar] ;
     
-    ctx.clearRect(mass_i.x * 300 - ( w_ / 2 )  , mass_i.y * 300 - ( h_ / 2 )  , w_  , h_  ) ;
+    ctx.clearRect(mass_i.x * scale - ( w_ / 2 )  , mass_i.y * scale - ( h_ / 2 )  , w_  , h_  ) ;
 
     
     mass_i.manifestation.draw(
-      mass_i.x * 300 ,
-      mass_i.y * 300
+      mass_i.x * scale ,
+      mass_i.y * scale
     ) ;
       
     draw_name( mass_i )
     objects.get_masses().forEach(mass => {
       if( mass.name !== mass_i.name ){
-        let x = mass_i.x + mass.x * 300 ;
-        let y = mass_i.y + mass.y * 300 ;
+        let x = mass_i.x + mass.x * scale ;
+        let y = mass_i.y + mass.y * scale ;
 
         if( x > rec_size[0] )rec_size[0] += x ;
         if( y > rec_size[1] )rec_size[1] += y ;
@@ -82,8 +83,8 @@ function Canvas () {
       
     if( dragging ) {
       ctx.beginPath() ;
-      ctx.moveTo( ( ox - ( w_ / 2 ) ) + mass_i.x * 300 , ( oy - ( h_ / 2 ) ) + mass_i.y * 300 ) ;
-      ctx.lineTo( ( ix - ( w_ / 2 ) ) + mass_i.x * 300 , ( iy - ( h_ / 2 ) ) + mass_i.y * 300 ) ;
+      ctx.moveTo( ( ox - ( w_ / 2 ) ) + mass_i.x * scale , ( oy - ( h_ / 2 ) ) + mass_i.y * scale ) ;
+      ctx.lineTo( ( ix - ( w_ / 2 ) ) + mass_i.x * scale , ( iy - ( h_ / 2 ) ) + mass_i.y * scale ) ;
       ctx.strokeStyle = '#3F98FF' ;
       ctx.stroke()
     }
--- src/windows/Help.js
+++ src/windows/Help.js
@@ -2,6 +2,7 @@ import React , { useState } from 'react' ;
 import Window from '../components/window/window' ;
 import Button from '../components/main_btn/main_btn' ;
 import Side_btn from '../components/side_btn/side_btn' ;
+import { scale } from '../windows/Options' ;
 
 const bt_c_s = 'calc( 95% - 1.25em )' ,
       btn_sty = { height : '2.5em' ,
@@ -19,9 +20,11 @@ function Help () {
         <Button evnt={() => set_vis(false)} c='var(--main-c)'>close</Button>
       </div>
     } >
-      <div><p>- units : kilograme , solar mass .</p></div>
-      <div><p>- time : 292 days for every second, might be less depends on the refresh rate of screens.</p></div>
-      <div><p>- This simulation is and has not been made to be 100% accurate , it is just a fun project where you can see what will happen if an exoplanet suddenly enters the solar system .  </p></div>
+      <div><p>- units : kg , earth mass and au .</p></div>
+      <div><p>- scale : 1 au = {scale}px .</p></div>
+      <div><p>- time : 292 days for every second .</p></div>
+      <div><p>- radius : 0.000015% of small size objects <br/>and 0.00000015% from the bigger ones .</p></div>
+      <div><p>- This simulation has't been made to be 100% accurate , just a fun project where you can see what will happen if an exoplanet suddenly enters the solar system .  </p></div>
       <div><p>- The Information given in the info tab are only the ones being applied on this envirement, therefore it requires more calculations to be used as real time data . </p></div>
       <div><p>- if you like to contribute : <a target='_blank' >https://github.com/ed-dinaoui/g.simulation</a> </p> </div>
     </Window>
